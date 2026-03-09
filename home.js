0ca87932e02510294b62df6dbdd0808f3741aa20
SADIA <you@example.com>
2026-03-09 12:53:29 +0600

    implement issue fetching with loader handling

--- home.js
+++ home.js
@@ -1,6 +1,31 @@
+// API Endpoints
 const apiALL = 'https://phi-lab-server.vercel.app/api/v1/lab/issues'
 const apiSingle = 'https://phi-lab-server.vercel.app/api/v1/lab/issue'
 const apiSearch = 'https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q='
 
+// Global variable
 let allIssues = [];
 let currentFilter = 'all';
+
+//  Fetch all issues from API
+async function fetchIssues() {
+  showLoader(true); // show loading animation
+  try {
+    const res = await fetch(apiALL);
+    const data = await res.json();
+    console.log('Fetched data:', data);
+    allIssues = data.data || data || [];
+    document.getElementById('total').textContent = allIssues.length;
+    renderIssues(allIssues);
+  } catch (err) {
+    console.error('Error fetching issues:', err);
+  } finally {
+    showLoader(false); // hide loader after fetch
+  }
+};
+
+// Loader toggle
+function showLoader(show) {
+  document.getElementById('loader').classList.toggle('hidden', !show);
+  document.getElementById('issuesGrid').classList.toggle('hidden', show);
+};
